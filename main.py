a36204931870ac8eb9eab9658c59ef86b8188a1a
Jezuke <jezaki48@gmail.com>
2023-09-19 10:36:36 -0700

    added flask frontend

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from groupy.client import Client
 import os
+from flask import Flask, redirect, url_for, request
+from groupy.client import Client
 
 TOKEN = os.getenv('TOKEN')
 client = Client.from_token(TOKEN)
+
+app = Flask(__name__)
 
 def get_nickname(member):
     return member.nickname
@@ -10,17 +13,21 @@ def get_nickname(member):
 """
 Get a Member object by searching through group for keyword in nickname.
 """
+@app.route('/member/<name>')
 def get_member(group, keyword):
     group.members.sort(key=get_nickname)
     for member in group.members:
         if(keyword.lower() in member.nickname.lower()):
             return member
 
-def main():
-    group = client.groups.get(51796422) # Getting group object for Official Chat
-    member = get_member(group, "aquitania")
-    print(dir(member))
+@app.route('/search_member', methods=['POST'])
+def search_member():
+    if request.method == 'POST':
+        group = client.groups.get(51796422) # Getting group object for Official Chat
+        member_name = request.form['nm']
+        member = get_member(group, member_name)
+        return member.data
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
 
